e4156a87b1db6ebd8ad1b307cdadbad4029b3d88
Cheyenne Swarthout <Cheyenne Swarthout>
2016-11-01 23:20:11 -0400

    added adventure and resources directories

---
+++ adventure/actions.py
@@ -0,0 +1,70 @@
+import Player
+
+class Action():
+	def __init__(self, method, name, hotkey, **kwargs):
+		self.method = method
+		self.hotkey = hotkey
+		self.name = name
+		self.kwargs = kwargs
+		
+	def __str__(self):
+		return "{}: {}".format(self.hotkey, self.name)
+		
+class MoveNorth(Action):
+	def __init__(self):
+		super().__init__(
+			method=Player.move_north,
+			name='Move north',
+			hotkey='w'
+		)
+		
+class MoveSouth(Action):
+	def __init__(self):
+		super().__init__(
+			method=Player.move_south,
+			name='Move south',
+			hotkey='s'
+		)
+		
+class MoveEast(Action):
+	def __init__(self):
+		super().__init__(
+			method=Player.move_east,
+			name='Move east',
+			hotkey='d'
+		)
+		
+class MoveWest(Action):
+	def __init__(self):
+		super().__init__(
+			method=Player.move_west,
+			name='Move west',
+			hotkey='a'
+		)
+		
+class ViewInventory(Action):
+	"""Prints the player's inventory"""
+	def __init__(self):
+		super().__init__(
+			method=Player.print_inventory,
+			name='View inventory',
+			hotkey='i'
+		)
+		
+class Attack(Action):
+	def __init__(self, enemy):
+		super().__init__(
+			method=Player.attack,
+			name="Attack",
+			hotkey='e',
+			enemy=enemy
+		)
+		
+class Flee(Action):
+	def __init__(self, tile):
+		super().__init__(
+			method=Player.flee,
+			name="Flee",
+			hotkey='f',
+			tile=tile
+		)
---
+++ adventure/world.py
@@ -0,0 +1,18 @@
+_world = {}
+starting_position = (0, 0)
+
+def load_tiles():
+	"""Parses a file that describes the world pace into the _world object"""
+	with open('resources/map.txt', 'r') as f:
+		rows = f.readlines()
+	x_max = len(rows[0].split('\t')) #Assumes all rows contain the same number of tabs
+	for y in range(len(rows)):
+		cols = rows[y].split('\t')
+		for x in range(x_max):
+			if tile_name == 'StartingRoom':
+				global starting_position
+				starting_position = (x, y)
+			_world[(x, y)] = None if tile_name == '' else getattr(__import__('tiles'), tile_name)(x, y)
+			
+def tile_exists(x, y):
+	return _world.get((x, y))
